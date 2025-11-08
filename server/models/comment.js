8dea7632d9539e616f2892b57341d577a4c3bd3a
nwankwolinus <51293362+nwankwolinus@users.noreply.github.com>
2025-11-08 14:42:42 -0800

    Update comment.js

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,1 +1,6 @@
-
+const commentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  feed: { type: mongoose.Schema.Types.ObjectId, ref: "Feed", required: true },
+  text: { type: String, required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+}, { timestamps: true });
